7b701fdf6083508ac9645f774cee16aa38e70e43
eric-epsilla <eric@epsilla.com>
2023-07-25 00:36:28 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# pyepsilla
+# Epsilla Python SDK
+
+## 1.Installation
+```shell
+pip3 install pyepsilla
+```
+or
+```shell
+pip3 install --upgrade pyepsilla
+```
+
+## 2.Documentation
+
+
+## 3.FAQ
+
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pyepsilla',
+    version= '0.0.1',
+    keywords='epsilla',
+    author= 'Epsilla',
+    description='Epsilla Python SDK',
+    license='Apache License',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'requests>=2.19.1'
+    ],
+    url='https://github.com/epsilla-cloud/pyepsilla',
+    project_urls={
+        'Source': 'https://github.com/epsilla-cloud/pyepsilla',
+    },
+)
