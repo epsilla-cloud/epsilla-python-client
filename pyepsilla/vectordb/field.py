4578da1ee8197ecd6d36d5f6b4ad2c68fec6597e
eric-epsilla <eric@epsilla.com>
2023-07-28 01:09:22 +0800

    update pyepsilla

---
+++ pyepsilla/vectordb/field.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from enum import IntEnum
+
+class Filed():
+    def __init__(self, name, data_type, primary_key, dimensions, ):
+        pass
+
+class FieldType(IntEnum):
+    INT1 = 1  # TINYINT
+    INT2 = 2  # SMALLINT
+    INT4 = 3  # INT
+    INT8 = 4  # BIGINT
+    FLOAT = 10
+    DOUBLE = 11
+    STRING = 20
+    BOOL = 30
+
+    VECTOR_FLOAT = 40
+    VECTOR_DOUBLE = 41
+    UNKNOWN = 999
--- pyepsilla/vectordb/filed.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-
-class Filed():
-    def __init__(self, name, data_type, primary_key, dimensions, ):
-        pass
