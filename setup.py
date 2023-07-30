aafb2a94d56ebc9c33b198c4c721579db2de5ede
eric-epsilla <eric@epsilla.com>
2023-07-30 12:33:55 +0800

    update pyepsilla to 0.0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyepsilla',
-    version= '0.0.8',
+    version= '0.0.9',
     keywords='epsilla',
     author= 'Epsilla Team',
     description='Epsilla Python SDK',
