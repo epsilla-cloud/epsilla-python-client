e38ba414fa671556f1fade910d61b2a5de21d7b1
Eric <132873174+eric-epsilla@users.noreply.github.com>
2023-08-17 08:17:12 +0800

    Update gist-960-euclidean.py

--- examples/gist-960-euclidean.py
+++ examples/gist-960-euclidean.py
@@ -12,9 +12,9 @@ import h5py, datetime
 
 
 ## Connect to Epsilla vector database
-c = vectordb.Client(host='127.0.0.1', port='8888')
-c.load_db(db_name="benchmark", db_path="/tmp/epsilla", vector_scale=1000000, wal_enabled=False) ## pay attention to change db_path to persistent volume for production environment
-c.use_db(db_name="benchmark")
+client = vectordb.Client(host='127.0.0.1', port='8888')
+client.load_db(db_name="benchmark", db_path="/tmp/epsilla", vector_scale=1000000, wal_enabled=False) ## pay attention to change db_path to persistent volume for production environment
+client.use_db(db_name="benchmark")
 
 ## Read gist-960-euclidean data from hdf5
 f = h5py.File('gist-960-euclidean.hdf5', 'r')
@@ -27,11 +27,11 @@ records_num, dimensions = training_data.shape
 id_field = {"name": "id", "dataType": "INT"}
 vec_field = {"name": "vector", "dataType": "VECTOR_FLOAT", "dimensions": dimensions}
 fields = [id_field, vec_field]
-status_code, response = c.create_table(table_name="benchmark", table_fields=fields)
+status_code, response = client.create_table(table_name="benchmark", table_fields=fields)
 
 ## Insert 20000 data into table
 # records_data = [ {"id": i, "vector": training_data[i].tolist()} for i in range(20000)]
-# c.insert(table_name="benchmark", records=records_data)
+# client.insert(table_name="benchmark", records=records_data)
 
 ## Insert all data into table
 indexs = [ i for i in range(0, records_num+10000, 50000)]
@@ -40,12 +40,12 @@ for i in range(len(indexs)-1):
     start=datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
     # print(indexs[i], indexs[i+1])
     records_data = [{"id": i, "vector": training_data[i].tolist()} for i in range(indexs[i], indexs[i+1])]
-    c.insert(table_name="benchmark", records=records_data)
+    client.insert(table_name="benchmark", records=records_data)
     end = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
     print("START:", start, "\nEND  :", end)
 
 ## Rebuild ann graph, it will wait until rebuild is finished, wait time is depended on the amount of dataset
-c.rebuild()
+client.rebuild()
 
 ## Query Vectors
 query_field = "vector"
@@ -53,5 +53,5 @@ query_vector = training_data[40000].tolist()
 response_fields = ["id"]
 limit = 2
 
-status_code, response = c.query(table_name="benchmark", query_field=query_field, query_vector=query_vector, response_fields=response_fields, limit=limit, with_distance=True)
+status_code, response = client.query(table_name="benchmark", query_field=query_field, query_vector=query_vector, response_fields=response_fields, limit=limit, with_distance=True)
 print("Response:", response)
