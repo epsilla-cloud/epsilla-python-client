98dfe1afb4ea7aeacf0deffe226afeb66af46455
Eric <132873174+eric-epsilla@users.noreply.github.com>
2023-08-30 19:25:02 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@
 
 from setuptools import setup, find_packages
 
+try:
+    with open("./pyepsilla/vectordb/version.py") as f
+    version = f.read().split("'")[-2]
+except Exception as e:
+    print("Error when read version: ", e)
+
 setup(
     name='pyepsilla',
-    version=open("./pyepsilla/vectordb/version.py").read().split("'")[-2],
+    version=version,
     keywords='epsilla',
     author= 'Epsilla Team',
     description='Epsilla Python SDK',
