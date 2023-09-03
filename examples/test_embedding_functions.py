00b49776b360054ae5d02cee28063749388ceb1a
eric-epsilla <eric@epsilla.com>
2023-09-04 03:13:11 +0800

    update with more sentence embedding models

--- examples/test_embedding_functions.py
+++ examples/test_embedding_functions.py
@@ -3,15 +3,21 @@
 
 from pyepsilla.vectordb.utils import embedding_functions
 
+sentences = ['This framework generates embeddings for each input sentence',
+    'Sentences are passed as a list of string.',
+    'The quick brown fox jumps over the lazy dog.']
 
-sentences = ["This is an example sentence", "Each sentence is converted"]
-
-# default
+# Default embedding function
 default_ef = embedding_functions.DefaultEmbeddingFunction()
-embeddings = sentence_transformer_ef.__call__(sentences)
+embeddings = default_ef.__call__(sentences)
 print(embeddings)
 
 ## Specific Sentence Transformers model
+## https://www.sbert.net/docs/pretrained_models.html#sentence-embedding-models/
 sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
+embeddings = sentence_transformer_ef.__call__(sentences)
+print(embeddings)
+
+sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-mpnet-base-v2")
 embeddings = sentence_transformer_ef.__call__(sentences)
 print(embeddings)
