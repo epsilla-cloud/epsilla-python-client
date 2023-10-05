c0d9248ef3b9ce24e38a539ed5f021969661d8f4
Ubuntu <ubuntu@ip-10-200-15-158.us-west-2.compute.internal>
2023-10-05 02:33:17 +0000

    add epsilla client for epsilla cloud

---
+++ pyepsilla/cloud/sentry.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# Sentry collects crash reports and performance numbers
+# It is possible to turn off data collection using an environment variable named "SENTRY_DISABLE"
+
+import os, sys, platform, uuid, hashlib, socket, requests
+import sentry_sdk
+from sentry_sdk.integrations.atexit import AtexitIntegration
+from .version import __version__
+
+CONFIG_URL = "https://config.epsilla.com/candidate.json"
+SENTRY_DSN = "https://08e83d2f5bffd58154ba3d377c1b7195@o4505728621412352.ingest.sentry.io/4505763445538816"
+
+try:
+    r = requests.get(CONFIG_URL, headers={"Agent": "PyEpsilla Cloud Client"}, timeout=2)
+    if r.status_code == 200:
+        SENTRY_DSN = r.json()["pyepsilla"][0]
+except Exception:
+    pass
+
+def callback(pending, timeout):
+    sys.stderr.flush()
+        
+def get_external_ip() -> str:
+    try:
+        return requests.get("https://api.ipify.org", timeout=2).text
+    except Exception:
+        return "NA"
+
+def init_sentry():
+    if "SENTRY_DISABLE" not in os.environ:
+        try:
+            uid = hashlib.sha256(str(uuid.getnode()).encode()).hexdigest()
+            internal_ip = socket.gethostbyname(socket.gethostname())
+            external_ip = get_external_ip()
+            sentry_sdk.set_tag("uid", uid)
+            sentry_sdk.set_tag("internal_ip", internal_ip)
+            sentry_sdk.set_tag("external_ip", external_ip)
+            sentry_sdk.set_user({'ip_address': '{{auto}}'})
+            sentry_sdk.set_user({"username": "{}-{}-{}".format(socket.gethostname(), internal_ip, external_ip)})
+            sentry_sdk.set_tag("version", platform.version())
+            sentry_sdk.set_tag("platform", "{}-{}".format(sys.platform, platform.machine()))
+            sentry_sdk.init(
+                dsn=SENTRY_DSN,
+                release=__version__,
+                traces_sample_rate=1.0,
+                integrations=[AtexitIntegration(callback=callback)],
+            )
+
+            sentry_sdk.capture_message("PyEpsilla Cloud Client Init", "info")
+        except Exception:
+            sentry_sdk.flush()
+            pass
