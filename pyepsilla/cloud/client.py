75aed5049d3f6d019f2cdc94871d1dd454704dd1
epsilla-eric <eric@epsilla.com>
2023-10-07 23:32:05 +0000

    add support for epsilla cloud

--- pyepsilla/cloud/client.py
+++ pyepsilla/cloud/client.py
@@ -8,9 +8,9 @@ import datetime
 
 class Client(Object):
     def __init__(self, projectid: str, apikey: str):
-        self._projectid = projectid
+        self._project_id = project_id
         self._apikey = apikey
-        self._baseurl = "https://api.cloud.epsilla.com"
+        self._baseurl = "https://controller.cloud.epsilla.com"
         self._db = None
         self._timeout = 10
         self._header = {'Content-type': 'application/json'}
@@ -23,141 +23,58 @@ class Client(Object):
         body = res.text
         return status_code, body
 
-    def state(self):
-        req_url = "{}/state".format(self._baseurl)
-        req_data = None
-        res = requests.get(url=req_url, data=json.dumps(req_data), headers=self._header)
-        status_code = res.status_code
-        body = res.json()
-        return status_code, body
 
-    def use_db(self, db_name: str):
-        self._db = db_name
 
-    def load_db(self, db_name: str, db_path: str, vector_scale: int = None, wal_enabled: bool = False):
-        req_url = "{}/api/load".format(self._baseurl)
-        req_data = {"name": db_name, "path": db_path}
-        if vector_scale is not None:
-            req_data["vectorScale"] = vector_scale
-        if wal_enabled is not None:
-            req_data["walEnabled"] = wal_enabled
-        res = requests.post(url=req_url, data=json.dumps(req_data), headers=self._header)
-        status_code = res.status_code
-        body = res.json()
-        return status_code, body
+    def get_all_projects(self):
+        pass
 
-    def unload_db(self, db_name: str):
-        req_url = "{}/api/{}/unload".format(self._baseurl, db_name)
-        res = requests.post(url=req_url, data=None, headers=self._header)
-        status_code = res.status_code
-        body = res.json()
-        return status_code, body
+    def get_project(self):
+        project_id: str = self._project_id
+        pass
 
-    def create_table(self, table_name: str = "MyTable", table_fields: list[str] = None):
-        if self._db is None:
-            raise Exception("[ERROR] Please use_db() first!")
-        if table_fields is None:
-            table_fields = []
-        req_url = "{}/api/{}/schema/tables".format(self._baseurl, self._db)
-        req_data = {"name": table_name, "fields": table_fields}
-        res = requests.post(url=req_url, data=json.dumps(req_data), headers=self._header)
-        status_code = res.status_code
-        body = res.json()
-        return status_code, body
+    def get_all_dbs(self):
+        project_id: str = self._project_id
+        pass
+
+    def get_db(self, db_uuid: str):
+        project_id: str = self._project_id
+        pass
+
+    def create_db(self, db_uuid: str):
+        pass
+
+    def delete_db(self, db_uuid: str):
+        pass
+
+    def load_db(self, db_uuid: str):
+        pass
+
+    def unload_db(self, db_uuid: str):
+        pass
+
 
     def list_tables(self):
-        if self._db is None:
-            raise Exception("[ERROR] Please use_db() first!")
-        req_url = "{}/api/{}/schema/tables/show".format(self._baseurl, self._db)
-        res = requests.get(url=req_url, headers=self._header)
-        status_code = res.status_code
-        body = res.json()
-        return status_code, body
+        pass
+
+
+    def create_table(self, table_name: str = "MyTable", table_fields: list[str] = None):
+        pass
+
 
     def insert(self, table_name: str = "MyTable", records: list = None):
-        if self._db is None:
-            raise Exception("[ERROR] Please use_db() first!")
-        if records is None:
-            records = []
-        req_url = "{}/api/{}/data/insert".format(self._baseurl, self._db)
-        req_data = {"table": table_name, "data": records}
-        res = requests.post(url=req_url, data=json.dumps(req_data), headers=self._header)
-        status_code = res.status_code
-        body = res.json()
-        return status_code, body
+        pass
 
     def delete(self, table_name: str = "MyTable", ids: list[str | int] = None):
-        """Epsilla only supports delete records by primay keys for now."""
-        if self._db is None:
-            raise Exception("[ERROR] Please use_db() first!")
-        if ids is None:
-            raise Exception("[ERROR] Please provide primary key list to delete record(s).")
-        req_url = "{}/api/{}/data/delete".format(self._baseurl, self._db)
-        req_data = {"table": table_name, "primaryKeys": ids}
-        res = requests.post(url=req_url, data=json.dumps(req_data), headers=self._header)
-        status_code = res.status_code
-        body = res.json()
-        return status_code, body
+        pass
 
-    def rebuild(self, timeout: int = 7200):
-        req_url = "{}/api/rebuild".format(self._baseurl)
-        req_data = None
-        print("[INFO] waiting until rebuild is finished ...")
-        start_time = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-        res = requests.post(url=req_url, data=json.dumps(req_data), headers=self._header, timeout=timeout)
-        end_time = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-        print("[INFO] Start Time:{}\n       End   Time:{}".format(start_time, end_time))
-        status_code = res.status_code
-        body = res.json()
-        return status_code, body
-
-    def query(
-        self, table_name: str = "MyTable",
-        query_field: str = "",
-        query_vector: list = None,
-        response_fields: list = None,
-        limit: int = 1,
-        with_distance: bool = False
-    ):
-        if self._db is None:
-            raise Exception("[ERROR] Please use_db() first!")
-        if query_vector is None:
-            query_vector = []
-        if response_fields is None:
-            response_fields = []
-        req_url = "{}/api/{}/data/query".format(self._baseurl, self._db)
-        req_data = {"table": table_name, "queryField": query_field, "queryVector": query_vector,
-                    "response": response_fields, "limit": limit, "withDistance": with_distance}
-        res = requests.post(url=req_url, data=json.dumps(req_data), headers=self._header)
-        status_code = res.status_code
-        body = res.json()
-        return status_code, body
-
-    def get(self, table_name: str = "MyTable", response_fields: list = None):
-        if self._db is None:
-            raise Exception("[ERROR] Please use_db() first!")
-        if response_fields is None:
-            response_fields = []
-        req_url = "{}/api/{}/data/get".format(self._baseurl, self._db)
-        req_data = {"table": table_name, "response": response_fields}
-        res = requests.post(url=req_url, data=json.dumps(req_data), headers=self._header)
-        status_code = res.status_code
-        body = res.json()
-        return status_code, body
+    def query(self, table_name: str = "MyTable", query_field: str = "", query_vector: list = None, response_fields: list = None, limit: int = 1, with_distance: bool = False):
+        pass
 
     def drop_table(self, table_name: str = None):
-        if self._db is None:
-            raise Exception("[ERROR] Please use_db() first!")
-        req_url = "{}/api/{}/schema/tables/{}".format(self._baseurl, self._db, table_name)
-        req_data = None
-        res = requests.delete(url=req_url, data=json.dumps(req_data), headers=self._header)
-        status_code = res.status_code
-        body = res.json()
-        return status_code, body
+        pass
 
-    def drop_db(self, db_name: str):
-        req_url = "{}/api/{}/drop".format(self._baseurl, db_name)
-        res = requests.delete(url=req_url, data=None, headers=self._header)
-        status_code = res.status_code
-        body = res.json()
-        return status_code, body
+    def drop_db(self, db_uuid: str):
+        pass
+
+
+
