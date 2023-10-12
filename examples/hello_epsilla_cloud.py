24f3e93a2675c97f4675c633f385a44fe9d756fd
epsilla-eric <eric@epsilla.com>
2023-10-12 23:11:16 +0000

    update table name

--- examples/hello_epsilla_cloud.py
+++ examples/hello_epsilla_cloud.py
@@ -21,7 +21,7 @@ db = client.vectordb(db_id="df7431d0-806b-4654-8b45-4bdb20038e26")
 
 # Insert new vector records into table
 status_code, response = db.insert(
-  table_name="MyTable2",
+  table_name="MyTable",
   records=[
     {"ID": 1, "Doc": "Berlin", "Embedding": [0.05, 0.61, 0.76, 0.74]},
     {"ID": 2, "Doc": "London", "Embedding": [0.19, 0.81, 0.75, 0.11]},
@@ -34,7 +34,7 @@ print(response)
 
 # Query Vectors with specific response field, otherwise it will return all fields
 status_code, response = db.query(
-  table_name="MyTable2",
+  table_name="MyTable",
   query_field="Embedding",
   query_vector=[0.35, 0.55, 0.47, 0.94],
   response_fields = ["Doc"],
@@ -45,7 +45,7 @@ print(response)
 
 # Delete specific records from table
 status_code, response = db.delete(
-  table_name="MyTable2", 
+  table_name="MyTable", 
   primary_keys=[4, 5]
 )
 print(response)
