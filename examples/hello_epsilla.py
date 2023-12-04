16ae04e22dd6c6e72142d3e4131ddc6bde8eb5fd
eric-epsilla <eric@epsilla.com>
2023-12-04 02:00:06 +0000

    fix the issue of limit for vectordb

--- examples/hello_epsilla.py
+++ examples/hello_epsilla.py
@@ -10,7 +10,7 @@
 from pyepsilla import vectordb
 
 # Connect to Epsilla VectorDB
-client = vectordb.Client(protocol='http',  host='127.0.0.1',        port='8888')
+client = vectordb.Client(protocol="http", host="127.0.0.1", port="8888")
 
 # You can also use Epsilla Cloud
 # client = vectordb.Client(protocol='https', host='demo.epsilla.com', port='443')
@@ -25,12 +25,12 @@ client.use_db(db_name="MyDB")
 
 # Create a table with schema in current DB
 status_code, response = client.create_table(
-  table_name="MyTable",
-  table_fields=[
-    {"name": "ID", "dataType": "INT", "primaryKey": True},
-    {"name": "Doc", "dataType": "STRING"},
-    {"name": "Embedding", "dataType": "VECTOR_FLOAT", "dimensions": 4}
-  ]
+    table_name="MyTable",
+    table_fields=[
+        {"name": "ID", "dataType": "INT", "primaryKey": True},
+        {"name": "Doc", "dataType": "STRING"},
+        {"name": "Embedding", "dataType": "VECTOR_FLOAT", "dimensions": 4},
+    ],
 )
 print(response)
 
@@ -40,41 +40,43 @@ print(response)
 
 # Insert new vector records into table
 status_code, response = client.insert(
-  table_name="MyTable",
-  records=[
-    {"ID": 1, "Doc": "Berlin", "Embedding": [0.05, 0.61, 0.76, 0.74]},
-    {"ID": 2, "Doc": "London", "Embedding": [0.19, 0.81, 0.75, 0.11]},
-    {"ID": 3, "Doc": "Moscow", "Embedding": [0.36, 0.55, 0.47, 0.94]},
-    {"ID": 4, "Doc": "San Francisco", "Embedding": [0.18, 0.01, 0.85, 0.80]},
-    {"ID": 5, "Doc": "Shanghai", "Embedding": [0.24, 0.18, 0.22, 0.44]}
-  ]
+    table_name="MyTable",
+    records=[
+        {"ID": 1, "Doc": "Berlin", "Embedding": [0.05, 0.61, 0.76, 0.74]},
+        {"ID": 2, "Doc": "London", "Embedding": [0.19, 0.81, 0.75, 0.11]},
+        {"ID": 3, "Doc": "Moscow", "Embedding": [0.36, 0.55, 0.47, 0.94]},
+        {"ID": 4, "Doc": "San Francisco", "Embedding": [0.18, 0.01, 0.85, 0.80]},
+        {"ID": 5, "Doc": "Shanghai", "Embedding": [0.24, 0.18, 0.22, 0.44]},
+    ],
 )
 print(response)
 
 # Query Vectors with specific response field
 status_code, response = client.query(
-  table_name="MyTable",
-  query_field="Embedding",
-  query_vector=[0.35, 0.55, 0.47, 0.94],
-  response_fields = ["Doc"],
-  limit=2
+    table_name="MyTable",
+    query_field="Embedding",
+    query_vector=[0.35, 0.55, 0.47, 0.94],
+    response_fields=["Doc"],
+    limit=2,
 )
 
 # Query Vectors without specific response field, then it will return all fields
 status_code, response = client.query(
-  table_name="MyTable",
-  query_field="Embedding",
-  query_vector=[0.35, 0.55, 0.47, 0.94],
-  limit=2
+    table_name="MyTable",
+    query_field="Embedding",
+    query_vector=[0.35, 0.55, 0.47, 0.94],
+    limit=2,
 )
 print(response)
 
-
-# status_code, response =  client.delete(table_name="MyTable", ids=[3])
-status_code, response =  client.delete(table_name="MyTable", primary_keys=[3, 4])
-# status_code, response =  client.delete(table_name="MyTable", filter="Doc <> 'San Francisco'")
+# Get Vectors
+status_code, response = client.get(table_name="MyTable", limit=2)
 print(response)
 
+# status_code, response =  client.delete(table_name="MyTable", ids=[3])
+status_code, response = client.delete(table_name="MyTable", primary_keys=[3, 4])
+# status_code, response =  client.delete(table_name="MyTable", filter="Doc <> 'San Francisco'")
+print(response)
 
 
 # Drop table
